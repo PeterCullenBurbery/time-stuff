a21780729e2608dba684ddc970a675811811eff1
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 14:52:50 -0400

    Add Time Zone Converter sample project
    Initial commit of a C# console application using the TimeZoneConverter package to list all known IANA time zone names. Includes solution, project, and main program files targeting .NET 9.0.

---
+++ c-sharp-projects/list-time-zones/Time_Zone_Converter_package/Time_Zone_Converter_package/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Time_Zone_Converter_package;
+
+using System;
+using TimeZoneConverter;
+
+class Program
+{
+    static void Main()
+    {
+        foreach (var id in TZConvert.KnownIanaTimeZoneNames)
+            Console.WriteLine(id);
+    }
+}

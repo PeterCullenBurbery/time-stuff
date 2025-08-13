e5d8eef4c05016c2ea495a5387d143fa10cb17d7
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 15:53:31 -0400

    Add initial time zone conversion project
    Creates a new C# console application to display the local IANA and Windows time zone IDs. Includes solution and project files, and references NodaTime and TimeZoneConverter packages for time zone handling.

---
+++ c-sharp-projects/get-time-zone/Get_Time_zones/Get_time_zones/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Get_time_zones;
+
+using System;
+using NodaTime;
+using TimeZoneConverter;
+
+class Program
+{
+    static (string iana, string windows) GetTimeZone()
+    {
+        // Get the local IANA time zone ID
+        string iana = DateTimeZoneProviders.Tzdb.GetSystemDefault().Id;
+
+        // Convert to the Windows time zone ID
+        string windows = TZConvert.IanaToWindows(iana);
+
+        return (iana, windows);
+    }
+
+    static void Main()
+    {
+        var (iana, windows) = GetTimeZone();
+        Console.WriteLine($"Local IANA time zone:    {iana}");
+        Console.WriteLine($"Local Windows time zone: {windows}");
+    }
+}

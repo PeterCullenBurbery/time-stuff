c49b5f34ecb8c617e1504efea50c63c5e8aa94cc
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 14:55:57 -0400

    Add project to list and sort IANA time zones
    Introduces a new .NET project using TimeZoneConverter to list all known IANA time zone names in sorted order. Updates the solution file to include the new project.

---
+++ c-sharp-projects/list-time-zones/Time_Zone_Converter_package/Time_Zone_Converter_list_Time_Zones_package_full_stop_sorted/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Time_Zone_Converter_list_Time_Zones_package_full_stop_sorted;
+
+using System;
+using System.Linq;
+using TimeZoneConverter;
+
+class Program
+{
+    static void Main()
+    {
+        foreach (var id in TZConvert.KnownIanaTimeZoneNames.OrderBy(x => x))
+            Console.WriteLine(id);
+    }
+}

180ee718339faa7c39277715a1dfe4d3e671a0a1
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 16:29:35 -0400

    Add IANA time zone project using NodaTime
    Introduced a new project 'Get_time_zones_IANA_tz_only' targeting .NET 9.0 and referencing NodaTime. The initial implementation retrieves and prints the system's local IANA time zone using NodaTime's Tzdb provider.

---
+++ c-sharp-projects/get-time-zone/Get_Time_zones/Get_time_zones_IANA_tz_only/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Get_time_zones_IANA_tz_only;
+
+using System;
+using NodaTime;
+
+class Program
+{
+    static DateTimeZone GetTimeZone()
+    {
+        // Returns the system's local time zone from Noda Time's provider
+        return DateTimeZoneProviders.Tzdb.GetSystemDefault();
+    }
+
+    static void Main()
+    {
+        var tz = GetTimeZone();
+        Console.WriteLine($"Local time zone: {tz.Id}");
+    }
+}

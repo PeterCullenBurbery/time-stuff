4760cb17ecc57401bcd35271f3310c37781d4fe3
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 12:28:13 -0400

    Add NodaTime time zone listing project
    Introduces a new console project using NodaTime to list canonical time zone IDs and display the tzdb version. Updates the solution file to include the new project.

---
+++ c-sharp-projects/Time_zones_solution/Noda_time_full_stop_Time_zones_project/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Noda_time_full_stop_Time_zones_project;
+
+using System;
+using NodaTime.TimeZones;
+
+class Program
+{
+    static void Main()
+    {
+        var source = TzdbDateTimeZoneSource.Default;
+        Console.WriteLine($"tzdb version: {source.VersionId}");
+
+        // Canonical IDs only (recommended for clean lists)
+        foreach (var id in source.CanonicalIdMap.Keys)
+            Console.WriteLine(id);
+    }
+}

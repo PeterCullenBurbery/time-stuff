7e0bea190384a1c84b3d2b367af0df51d3ecd217
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 12:14:51 -0400

    List all IANA time zones using TimeZoneConverter
    Added TimeZoneConverter NuGet package and updated Program.cs to enumerate all IANA time zone names, including conversion from Windows IDs and known IANA zones. The output is now a sorted list of IANA time zone names printed to the console.

--- c-sharp-projects/Time_zones_solution/List_time_zones_001_version/Program.cs
+++ c-sharp-projects/Time_zones_solution/List_time_zones_001_version/Program.cs
@@ -1,9 +1,43 @@
 ﻿namespace List_time_zones_001_version;
 
+using System;
+using System.Collections.Generic;
+using TimeZoneConverter;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        HashSet<string> ianaZones = new HashSet<string>();
+
+        // Get all system time zones
+        foreach (var tz in TimeZoneInfo.GetSystemTimeZones())
+        {
+            // Convert Windows ID to IANA ID
+            try
+            {
+                string iana = TZConvert.WindowsToIana(tz.Id);
+                ianaZones.Add(iana);
+            }
+            catch
+            {
+                // If already IANA or not convertible, skip
+            }
+        }
+
+        // Add common IANA zones (in case they don't exist in Windows mapping)
+        foreach (var iana in TZConvert.KnownIanaTimeZoneNames)
+        {
+            ianaZones.Add(iana);
+        }
+
+        // Sort and print
+        List<string> sortedZones = new List<string>(ianaZones);
+        sortedZones.Sort();
+
+        foreach (var zone in sortedZones)
+        {
+            Console.WriteLine(zone);
+        }
     }
 }

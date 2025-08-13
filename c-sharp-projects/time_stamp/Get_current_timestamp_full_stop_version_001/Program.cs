02f688470b92bda43812e62cd09025729d31747d
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 16:33:45 -0400

    Add project to display current timestamp
    Initial commit for a C# console application that prints the current timestamp with time zone information. Includes project file, main program, and solution file setup.

---
+++ c-sharp-projects/time_stamp/Get_current_timestamp_full_stop_version_001/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Get_current_timestamp_full_stop_version_001;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Get current date and time with time zone info
+        DateTimeOffset timestamp = DateTimeOffset.Now;
+
+        Console.WriteLine("Current Timestamp: " + timestamp);
+    }
+}

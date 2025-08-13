ca4432296421912f0edaa8fb0b58c8f420507ec1
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 16:48:31 -0400

    Add project to print current date in YYYY_MM_DD format
    Introduces a new C# console project that outputs the current date formatted as YYYY_MM_DD. Updates the solution file to include the new project and its build configurations.

---
+++ c-sharp-projects/time_stamp/Get_current_timestamp_version_002_full_stop_YYYY_MM_DD_full_stop_standard/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Get_current_timestamp_version_002_full_stop_YYYY_MM_DD_full_stop_standard;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Get current date
+        DateTime now = DateTime.Now;
+
+        // Format as YYYY_MM_DD
+        string formattedDate = now.ToString("yyyy_MM_dd");
+
+        Console.WriteLine("Current Date: " + formattedDate);
+    }
+}

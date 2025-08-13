06cda7796c6a873e8db8525083968cf5c2de637a
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 15:34:41 -0400

    Add canonical time zones listing project
    Introduces a new project 'Noda_Time_list_Time_Zones_canonical' to the solution. This project lists canonical time zone IDs using NodaTime and targets .NET 9.0.

---
+++ c-sharp-projects/list-time-zones/Noda_Time_list_Time_Zones/Noda_Time_list_Time_Zones_canonical/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Noda_Time_list_Time_Zones_canonical;
+
+using System;
+using System.Linq;
+using NodaTime.TimeZones;
+
+class Program
+{
+    static void Main()
+    {
+        var source = TzdbDateTimeZoneSource.Default;
+        Console.WriteLine($"tzdb version: {source.VersionId}");
+
+        var canonicalIds = source.CanonicalIdMap
+            .Where(kv => kv.Key == kv.Value)                 // keep only canonical IDs
+            .Select(kv => kv.Key);
+
+        foreach (var id in canonicalIds)
+            Console.WriteLine(id);
+    }
+}
